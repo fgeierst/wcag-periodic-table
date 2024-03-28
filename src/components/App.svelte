f59ef57a1c2d24cf324f75dfd18136d5323c1a87
Florian Geierstanger <florian.geierstanger@codecentric.de>
2024-03-28 17:21:06 +0100

    Animation

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -3,10 +3,17 @@
   import Filter from "./Filter.svelte";
   import Card from "./Card.svelte";
   import { criteria, selectedCriteria } from "../stores.ts";
+  import { quadInOut } from "svelte/easing";
+  import { flip } from "svelte/animate";
+
+  let reducedMotion = false;
 
   onMount(async function () {
     const response = await fetch("/api/wcag.json");
     $criteria = await response.json();
+    reducedMotion = window.matchMedia(
+      "(prefers-reduced-motion: reduce)",
+    ).matches;
   });
 </script>
 
@@ -17,16 +24,23 @@
 <Filter />
 
 <div class="grid grid-cols-[repeat(auto-fill,minmax(7rem,1fr))] gap-3">
-  {#each $selectedCriteria as criterion}
-    <Card
-      {criterion}
-      backgroundColor={criterion.level === "A"
-        ? "lightgreen"
-        : criterion.level === "AA"
-          ? "yellow"
-          : criterion.level === "AAA"
-            ? "pink"
-            : ""}
-    />
+  {#each $selectedCriteria as criterion (criterion.ref_id)}
+    <div
+      animate:flip={{
+        duration: reducedMotion ? 0 : 600,
+        easing: quadInOut,
+      }}
+    >
+      <Card
+        {criterion}
+        backgroundColor={criterion.level === "A"
+          ? "lightgreen"
+          : criterion.level === "AA"
+            ? "yellow"
+            : criterion.level === "AAA"
+              ? "pink"
+              : ""}
+      />
+    </div>
   {/each}
 </div>
