a1c4fc70899fa740d3ab9f4ccc43ac9961f823a9
Florian Geierstanger <florian.geierstanger@codecentric.de>
2024-03-28 10:56:09 +0100

    Extract Filter component

---
+++ src/components/App.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import Filter from "./Filter.svelte";
+  import Card from "./Card.svelte";
+  import { criteria, selectedCriteria } from "../stores.ts";
+
+  onMount(async function () {
+    const response = await fetch("/api/wcag.json");
+    $criteria = await response.json();
+  });
+</script>
+
+<h1 class="text-lg font-bold">
+  WCAG 2.2 Success Criteria ({$selectedCriteria.length})
+</h1>
+
+<Filter />
+
+<div class="grid grid-cols-[repeat(auto-fill,minmax(7rem,1fr))] gap-3">
+  {#each $selectedCriteria as criterion}
+    <Card
+      {criterion}
+      backgroundColor={criterion.level === "A"
+        ? "lightgreen"
+        : criterion.level === "AA"
+          ? "yellow"
+          : criterion.level === "AAA"
+            ? "pink"
+            : ""}
+    />
+  {/each}
+</div>
--- src/components/Filter.svelte
+++ src/components/Filter.svelte
@@ -1,18 +1,7 @@
 <script lang="ts">
-  import { onMount } from "svelte";
   import { SortKey } from "../utils/sorting.ts";
-  import Card from "./Card.svelte";
-  import { criteria, options, selectedCriteria } from "../stores.ts";
-
-  onMount(async function () {
-    const response = await fetch("/api/wcag.json");
-    $criteria = await response.json();
-  });
+  import { options } from "../stores.ts";
 </script>
-
-<h1 class="text-lg font-bold">
-  WCAG 2.2 Success Criteria ({$selectedCriteria.length})
-</h1>
 
 <div class="my-3 flex gap-x-5 gap-y-1 gap flex-row flex-wrap">
   <div>
@@ -39,19 +28,4 @@
       {/each}
     </select>
   </div>
-</div>
-
-<div class="grid grid-cols-[repeat(auto-fill,minmax(7rem,1fr))] gap-3">
-  {#each $selectedCriteria as criterion}
-    <Card
-      {criterion}
-      backgroundColor={criterion.level === "A"
-        ? "lightgreen"
-        : criterion.level === "AA"
-          ? "yellow"
-          : criterion.level === "AAA"
-            ? "pink"
-            : ""}
-    />
-  {/each}
 </div>
